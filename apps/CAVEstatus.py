a7a7bc9094562211cf39ff4d4e4f3a5eec893b0a
Alessandro Febretti <febret@gmail.com>
2014-09-02 01:53:11 -0500

    NEW: apps dir with applications launchable using the MVI module

---
+++ apps/CAVEstatus.py
@@ -0,0 +1,102 @@
+from omega import *
+from omegaToolkit import *
+from webView import *
+from euclid import *
+
+#-------------------------------------------------------------------------------
+view = None
+container = None
+titleBar = None
+frame = None
+draggable = False
+width = 0
+height = 0
+
+# Scale, max width, max height
+S = 1 #Platform.scale
+MW = 1920
+MH = 1080
+scale = S
+    
+#-------------------------------------------------------------------------------
+def create(width, height):
+    parent = UiModule.createAndInitialize().getUi()
+    
+    global container
+    container = Container.create(ContainerLayout.LayoutFree, parent)
+    container.setAlpha(1)
+    #container.setFillColor(Color('black'))
+    #container.setFillEnabled(True)
+    container.setAutosize(False)
+    container.setMargin(0)
+    container.setSize(Vector2(width, height))
+    
+    global view, frame
+    #if(isMaster()):
+    view = WebView.create(width, height)
+    frame = WebFrame.create(container)
+    frame.setView(view)
+    # else:
+        # # Needs to be RGBA since that's the image format used by WebView.
+        # view = PixelData.create(width, height, PixelFormat.FormatRgba)
+        # frame = Image.create(container)
+        # frame.setData(view)
+        
+
+    frame.setPosition(Vector2(5, 5))
+    #frame.setSizeAnchorEnabled(True)
+    #frame.setSizeAnchor(Vector2(5, 5))
+    container.requestLayoutRefresh()
+    #ImageBroadcastModule.instance().addChannel(view, 'browser', ImageFormat.FormatNone)
+
+#-------------------------------------------------------------------------------
+def loadUrl(url):
+    print('loading ' + url)
+    view.loadUrl(url)
+
+#-------------------------------------------------------------------------------
+def rescale(newscale):
+    global scale
+    scale = newscale
+    resize(width - 5, height - 5)
+    
+#-------------------------------------------------------------------------------
+def resize(width, height):
+    sw = float(width) / scale
+    sh = float(height) / scale
+    view.resize(int(sw), int(sh))
+    container.setSize(Vector2(sw * scale, sh * scale))
+    
+    ox = (width - sw) / 2
+    oy = (height - sh) / 2
+    frame.setSize(Vector2(sw, sh))
+    frame.setScale(scale)
+    frame.setPosition(Vector2(5 + ox, 5 + oy))
+    print("width: " + str(width))
+    print("scale: " + str(scale))
+
+#-------------------------------------------------------------------------------
+def onCanvasChanged():
+    global width, height, scale
+    cr = getDisplayConfig().getCanvasRect()
+    w = cr[2]
+    h = cr[3]
+    if(w != width or h != height):
+        if(w > MW or h > MH):
+            if(w > h):
+                scale = S * w / MW
+            else:
+                scale = S * h / MH
+        else:
+            scale = S
+        width = w
+        height = h
+        rescale(scale)
+getDisplayConfig().canvasChangedCommand = 'onCanvasChanged()'
+    
+#-------------------------------------------------------------------------------
+#setClearColor(Color('black'))
+create(400,400)
+loadUrl('http://www.evl.uic.edu/aej/cavemonitor.html')
+onCanvasChanged()
+
